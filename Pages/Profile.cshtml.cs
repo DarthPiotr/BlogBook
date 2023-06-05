aba516511dd01030f62c0d9e6d0abe934b6c1550
DarthPiotr <vader.piotr@gmail.com>
2023-06-05 20:25:38 +0200

    Partial pages: posts

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -3,47 +3,64 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BlogBook.Pages
 {
-    public class ProfileModel : PageModel
-    {
-        public List<Post> Posts { get; set; }
+	public class ProfileModel : PageModel
+	{
+		public List<Post> Posts { get; set; }
+		public IdentityUser ProfileUser { get; set; }
 
-        private readonly BlogbookDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
+		private readonly BlogbookDbContext _context;
+		private readonly UserManager<IdentityUser> _userManager;
 
-        public ProfileModel(BlogbookDbContext context, UserManager<IdentityUser> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
+		public ProfileModel(BlogbookDbContext context, UserManager<IdentityUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
-        public async Task<IActionResult> OnGet(string? userName)
-        {  
-            if (string.IsNullOrEmpty(userName))
-            {
-                // Gdy nie podano parametru, przekieruj na aktualnego u¿ytkownika
-                var currentUser = await _userManager.GetUserAsync(User);
-                if (currentUser == null)
-                {
-                    return RedirectToPage("Index");
-                }
-                return RedirectToPage("Profile", new { userName = currentUser.UserName });
-            }
+		public async Task<IActionResult> OnGet(string? userName, string? query)
+		{
+			if (string.IsNullOrEmpty(userName))
+			{
+				// Gdy nie podano parametru, przekieruj na aktualnego u¿ytkownika
+				var currentUser = await _userManager.GetUserAsync(User);
+				if (currentUser == null)
+				{
+					return RedirectToPage("Index");
+				}
+				return RedirectToPage("Profile", new { userName = currentUser.UserName });
+			}
 
-            var user = await _userManager.FindByNameAsync(userName);
-            if (user == null)
-            {
-                // Gdy podany u¿ytkownik nie istnieje, przekieruj na stronê domow¹
-                return RedirectToPage("Index");
-            }
+			ProfileUser = await _userManager.FindByNameAsync(userName);
+			if (ProfileUser == null)
+			{
+				// Gdy podany u¿ytkownik nie istnieje, przekieruj na stronê domow¹
+				return RedirectToPage("Index");
+			}
 
-            var blogbookDbContext = _context.Post
-                .Where(p => p.User.Id == user.Id)
-                .Include(p => p.User);
-            Posts = await blogbookDbContext.ToListAsync();
-            return Page();
-        }
-    }
+			if (!string.IsNullOrEmpty(query))
+			{
+				var filteredContext = _context.Post
+				.Where(p => p.User.Id == ProfileUser.Id)
+					.Where(p => EF.Functions.Like(p.Content, $"%{query}%") ||
+								EF.Functions.Like(p.Title, $"%{query}%") ||
+								EF.Functions.Like(p.User.UserName, $"%{query}%"))
+				.Include(p => p.User)
+				.OrderByDescending(p => p.PostDate);
+				Posts = await filteredContext.ToListAsync();
+
+				return Page();
+			}
+
+			var blogbookDbContext = _context.Post
+				.Where(p => p.User.Id == ProfileUser.Id)
+				.Include(p => p.User)
+				.OrderByDescending(p => p.PostDate);
+			Posts = await blogbookDbContext.ToListAsync();
+			return Page();
+		}
+	}
 }
